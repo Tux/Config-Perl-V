4126cd17cef3343047902b9b0086e8a482adf970
H.Merijn Brand - Tux <h.m.brand@xs4all.nl>
2018-01-01 14:14:01 +0100

    It's 2018

--- V.pm
+++ V.pm
@@ -6,7 +6,7 @@ use warnings;
 use Config;
 use Exporter;
 use vars qw($VERSION @ISA @EXPORT_OK %EXPORT_TAGS);
-$VERSION     = "0.29";
+$VERSION     = "0.30";
 @ISA         = qw( Exporter );
 @EXPORT_OK   = qw( plv2hash summary myconfig signature );
 %EXPORT_TAGS = (
@@ -552,7 +552,7 @@ H.Merijn Brand <h.m.brand@xs4all.nl>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2009-2017 H.Merijn Brand
+Copyright (C) 2009-2018 H.Merijn Brand
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
