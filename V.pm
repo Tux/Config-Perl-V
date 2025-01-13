9c359bf398028f761345a7dd56de5cc75ebac0c1
H.Merijn Brand - Tux <linux@tux.freedom.nl>
2025-01-13 11:32:43 +0100

    Improve static doc generation

--- V.pm
+++ V.pm
@@ -575,7 +575,7 @@ H.Merijn Brand <h.m.brand@xs4all.nl>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2009-2024 H.Merijn Brand
+Copyright (C) 2009-2025 H.Merijn Brand
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
